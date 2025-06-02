b21788c0b6862052a826776c8c2a9ed3a14a2f8a
Alekei <aleksei.nesterinen@outlook.com>
2025-06-02 12:27:24 +0300

    readme, versioning

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-<h1>TODO</h1>
+<h1 align="center">Vuosikello</h1>
+
 <p align="center"><img src="dev/vuosikello.png"/></p>
+
+<a href="https://codeload.github.com/nesterinen/VuosiKello/zip/refs/heads/main"> Download(.zip) </a>
--- Vuosi.php
+++ Vuosi.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name: VuosiKello
  * Description: Organisaation tapahtumien yleiskatsaus vuosikello muodossa.
- * Version: 0.9
+ * Version: 0.9.5
  * Author: Aleksei Nesterinen
- * Author URI: none
-* Plugin URI: none
+ * Author URI: https://github.com/nesterinen
+ * Plugin URI: https://codeload.github.com/nesterinen/VuosiKello/zip/refs/heads/main
 */
 
 if (!defined(constant_name: 'ABSPATH')) {
@@ -103,7 +103,7 @@ register_activation_hook(__FILE__, 'vuosi_kello_plugin_activation');
 // ajax, rest/crud api modifying db stuff for vuosikello
 include(plugin_dir_path(__FILE__) . 'php/Crud_Operations.php');
 function load_plugin(): void{
-    $version = '0.9';
+    $version = '0.9.5';
 
     global $vuosi_kello_page_name;
     if(!is_page($vuosi_kello_page_name)){
